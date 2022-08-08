3f5b9cac4fbb6259fc54dd7e08c36f82f8383dd3
Sanaria Fadel <103165467+Sanaria1@users.noreply.github.com>
2022-08-07 22:48:02 -0500

    Update simple_label.py

--- simple_label.py
+++ simple_label.py
@@ -25,21 +25,21 @@ from tkinter import *
 
 window = Tk()
 
-photo = PhotoImage(file=" Python") #put the image path in section" " 
+photo = PhotoImage(file= "Python") #put the image path in this section " " 
 
 label = Label(window,
               text="Hello Python",
-              font=('Arial',40,'bold'),
-              fg='#00FF00',
-              bg='black',
+              font=("Aril", 40, "bold"),
+              fg="pink",
+              bg="black",
               relief=RAISED,
               bd=10,
               padx=20,
               pady=20,
               image=photo,
-              compound='bottom')
+              compound="bottom") #to place the image in the bottom of the label. 
 label.pack()
-#label.place(x=0,y=0)
+#label.place(x=0,y=0) 
 
 window.mainloop()
   
